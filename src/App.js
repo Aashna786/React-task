a3b53e6ffae018f74ec5f0315ec6f8a562b679e2
Repository-3 <200shaikasha@gmail.com>
2024-05-22 12:09:04 +0530

    dynamic-routing-extension task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
+import CartProvider from "./static-routes/cart-context";
 import Navs from "./static-routes/navsection";
 
 
 const App=()=> {
   return(
 
+    <CartProvider>
     <div className='App'>
       
 {/* <HandleNavigation/> */}
 {/* <Mounting></Mounting> */}
 <Navs></Navs>
 
-  
 </div>
+</CartProvider>
   )
 }
 
--- src/static-routes/productDetails.js
+++ src/static-routes/productDetails.js
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import NavBar from "./navbar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Spinner from "react-bootstrap/Spinner";
+import { CartContext } from "./cart-context";
+import { Alert } from "react-bootstrap";
 
 const EachProd = () => {
   const { id } = useParams();
-
-  console.log(id);
-
+  const {addToCart}=useContext(CartContext)
   const [eachprod, setEachprod] = useState({});
+  const[showAlert,setShowAlert]=useState(false);
+
 
   const { title, description, price, brand, category, thumbnail } = eachprod;
 
@@ -19,16 +21,25 @@ const EachProd = () => {
     fetchProd();
   }, [id]);
 
+
   const fetchProd = async () => {
     const res = await axios.get(`https://dummyjson.com/products/${id}`);
+    console.log('Product data fetched:', res.data);
     setEachprod(res.data);
   };
-  console.log(eachprod);
+
+const handleAddToCart=()=>{
+  console.log('Adding to cart:',eachprod);
+  addToCart(eachprod);
+  setShowAlert(true);
+  setTimeout(()=>setShowAlert(false),3000);
+};
 
   return (
     <>
       <NavBar />
-      <>
+<div className="container mt-5">
+
         {Object.keys(eachprod).length > 0 ? (
           <Card className="text-center">
             <Card.Body>
@@ -44,7 +55,13 @@ const EachProd = () => {
               <Card.Text><b>Brand: </b>{brand}</Card.Text>
               <Card.Text><b>Category: </b>{category}</Card.Text>
               <Card.Text><b>Price: </b>${price}</Card.Text>
-              <Button variant="outline-success">Add to Cart</Button>
+              <Button variant="outline-success"  onClick={handleAddToCart}>Add to Cart</Button>
+            
+            {showAlert && (
+              <Alert variant="success" onClose={()=>setShowAlert(false)} dismissible>
+                Product has been added to your Cart!
+              </Alert>
+            )}
             </Card.Body>
           </Card>
         ) : (
@@ -58,8 +75,8 @@ const EachProd = () => {
             variant="success"
           />
         )}
+</div>
       </>
-    </>
   );
 };
 
